8dacef22086f9274e03275194ba3261f7a438337
liushuhao <794516020@qq.com>
2020-12-28 13:28:41 +0800

    feat： add bind

--- bind.js
+++ bind.js
@@ -1,13 +1,24 @@
-/*
- * @Description: 
- * @Author: liushuhao
- * @Date: 2020-12-25 16:44:15
- * @LastEditors: liushuhao
- */
-// bind 除了绑定 this 外, 还有一个用法是使一个函数拥有预设的初始参数，就像偏函数。
+// bind 的使用
+var obj = {
+    age: 18,
+    myFun: function (a, b, c) {
+        console.log('this', this);
+        console.log(a, b, c)
+    }
+}
 
+var db = { name: 1 }
+let bound = obj.myFun.bind(db, '1', '2')
+console.log(bound('3'))  // 偏函数   
+
+// bind 绑定 this 
+// bind 是一个偏函数
 Function.prototype.bind = function() {
+    var thatFn = this
+    var context = arguments[0]
+    var args = [].slice.call(arguments, 1)
     return function () {
-        
+        var argsList = args.concat([].slice.call(arguments))  // 合并 bind 的参数和调用传入的参数
+        thatFn.apply(context, argsList)
     }
 }
